3ba6b557d8901aa17d434e1f278d01c0b5bd5340
bamohammad <bamohammad@clickapps.co>
2016-11-11 11:37:13 +0530

    add border button

--- SnapCamRecorder/ViewController.swift
+++ SnapCamRecorder/ViewController.swift
@@ -13,47 +13,44 @@ class ViewController: UIViewController {
     @IBOutlet var circleButton: UIButton!
     @IBOutlet var vwRecored: UIView!
     var circle: CAShapeLayer!
+    var buttonBorder: CAShapeLayer!
     var circleLayer: CAShapeLayer!
     var drawAnimation: CABasicAnimation!
-   
     let buttnSize:CGFloat = 70
-    //@IBOutlet var vwCircle: UIView!
+
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        vwRecored.alpha = 0
+
         configureCircle()
     }
 
     
     func configureCircle() {
         
-//        self.VwRecored.layer.cornerRadius = self.VwRecored.frame.size.width / 2
-//        self.VwRecored.clipsToBounds = true
+
         let radius:CGFloat = 33
         self.circle = CAShapeLayer()
+        self.buttonBorder = CAShapeLayer()
         
         // Make a circular shape
         self.circle.path = UIBezierPath(roundedRect: CGRect(x: 0, y: 0, width: radius * 2.0, height: radius * 2.0), cornerRadius: CGFloat(radius)).cgPath
+        // Make a circular border
+        self.buttonBorder.path = UIBezierPath(roundedRect: CGRect(x: 0, y: 0, width: radius * 2.0, height: radius * 2.0), cornerRadius: CGFloat(radius)).cgPath
        
         
         self.circleButton.frame.size = CGSize(width: buttnSize, height: buttnSize)
         
-        // Configure the apperence of the red circle
-       /* self.vwRecored.alpha = 0.0
-        self.vwRecored.frame.size = CGSize(width: buttnSize , height: buttnSize  )
-        self.vwRecored.layoutIfNeeded()
-        self.vwRecored.layer.cornerRadius = self.vwRecored.frame.size.width / 2
-                self.vwRecored.clipsToBounds = true
-        */
+        //  make button in circl shape
+        self.circleButton.layer.cornerRadius = self.circleButton.bounds.width / 2
+        self.circleButton.clipsToBounds = true
+
         
         self.circle.position =  CGPoint (x: self.circleButton.frame.midX - radius, y: self.circleButton.frame.midY - radius)
-        
-        //(x: self.circleButton.frame.origin.x + 10 , y: self.circleButton.frame.origin.y + 10)
-        
+        self.buttonBorder.position =  CGPoint (x: self.circleButton.frame.midX - radius, y: self.circleButton.frame.midY - radius)
+
         // Configure the apperence of the circle
         self.circle.fillColor = UIColor.clear.cgColor
         self.circle.strokeColor = UIColor.red.cgColor
@@ -61,12 +58,20 @@ class ViewController: UIViewController {
         
         self.circle.strokeEnd = 0
         
+        
+        // Configure the apperence of the button burder
+        self.buttonBorder.fillColor = UIColor.clear.cgColor
+        self.buttonBorder.strokeColor = UIColor.lightGray.cgColor
+        self.buttonBorder.lineWidth = 5
+        
+        
         // Configure the apperence of the circleButton
         
         self.circleButton.tintColor = UIColor.gray
         
         
         // Add to parent layer
+        self.circleButton.layer.addSublayer(self.buttonBorder)
         self.circleButton.layer.addSublayer(self.circle)
         
         // Target for touch down (hold down)
@@ -103,16 +108,23 @@ class ViewController: UIViewController {
         self.drawAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn)
         self.circle.add(self.drawAnimation, forKey: "draw")
         
-        vwRecored.alpha = 0
+        // Configure animation
+        let fromValue = 50.0
+        let toValue =  0
+        CATransaction.setDisableActions(true)
+        self.circleLayer.bounds.size.height = CGFloat(toValue)
+        let positionAnimation = CABasicAnimation(keyPath: "path")
+        positionAnimation.fromValue = fromValue
+        positionAnimation.toValue = toValue
+        positionAnimation.duration = 6.0
+        positionAnimation.repeatCount = 1
+        self.circleLayer.add(positionAnimation, forKey: "bounds")
+        
 
-        /*
-         var frame = CGRect(x: self.circleButton.frame.origin.x - 5, y: self.circleButton.frame.origin.y - 5, width: self.circleButton.frame.size.width + 5, height: self.circleButton.frame.size.height + 5)
-        */
-         //self.vwRecored.transform = CGAffineTransform(scaleX: 0, y: 0)
-        UIView.animate(withDuration: 0.7, animations: {
+        UIView.animate(withDuration: 1, animations: {
             
            self.circleButton.transform = CGAffineTransform(scaleX: 1.15,y: 1.15);
-            //self.vwRecored.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+           
             
             }, completion: nil)
         
@@ -139,11 +151,10 @@ class ViewController: UIViewController {
         UIView.animate(withDuration: 0.5, animations: {
             
             self.circleButton.transform = CGAffineTransform(scaleX: 1,y: 1);
-            //self.vwRecored.transform = CGAffineTransform(scaleX: 0, y: 0)
+           
             
             }, completion: { (finished) -> Void in
-                // ....
-               // self.vwRecored.alpha = 0
+
                 
         })
         
